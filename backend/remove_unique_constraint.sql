04d82d342a632cfa5febcdcc7404d37c3ee0f400
zaleoz1 <isaleoguimaraes284@gmail.com>
2026-01-11 13:05:36 -0300

    Dev Zaleoz1 concertando as macacada do Kaio

--- backend/remove_unique_constraint.sql
+++
@@ -1,8 +0,0 @@
--- Script para remover a constraint única do carrinho
--- Execute este comando diretamente no MySQL
-
-USE acai_db;
-
--- Remover a constraint única que impede múltiplos itens do mesmo produto no carrinho
-ALTER TABLE `itens_carrinho` DROP INDEX `CartItem_cartId_productId_key`;
-
